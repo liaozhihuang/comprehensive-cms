503173470c4aea644377ec78c07e365c28df454f
liaozhihuang <13825341906@163.com>
2021-04-02 17:11:35 +0800

    add:ad广告栏目数据库生成

---
+++ database/migrations/20210402090507_create_table_ad.php
@@ -0,0 +1,43 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class CreateTableAd extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table  =  $this->table('ad',array('comment'=>'广告','engine'=>'InnoDB'));
+        $table->addColumn('ad_name', 'string',array('limit'  =>  100,'comment' =>'名称','default'=>''))
+              ->addColumn('group_sign', 'string',array('limit'  =>  30,'comment' =>'所属分组标识','default'=>''))
+              ->addColumn('img_url', 'string',array('limit'  =>  255,'comment' =>'图片地址','default'=>''))
+              ->addColumn('sort', 'string',array('limit'  =>  11,'comment' =>'排序','default'=>''))
+              ->addColumn('status', 'string',array('limit'  =>  11,'comment' =>'状态','default'=>''))
+              ->addColumn('link', 'string',array('limit'  =>  255,'comment' =>'链接','default'=>''))
+              ->addColumn('add_time', 'string',array('limit'  =>  15,'comment' =>'添加时间','default'=>0))
+              ->addColumn('delete_time', 'string',array('limit'  =>  15,'comment' =>'删除时间','null'=>true))
+              ->addIndex('group_sign')
+              ->create();
+    }
+}
